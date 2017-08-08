1d6d32c0e8d51b4aed768922d35acd20a7b4443e
Pavel Khodos <pkhodos@odin.com>
2017-08-08 20:09:44 +0300

    Adding HealthCheck as a separate file

---
+++ FallballConnectorDotNet/Controllers/HealthCheckController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace APSConnector.Controllers
+{
+    [Route("/v1/")]
+    public class HealthCheckController : Controller
+    {
+        private Config _config;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger, IConfiguration config)
+        {
+            _config = new Config { logger = logger, config = config };
+        }
+
+        // GET /v1/
+        [HttpGet(Name = "Root")]
+        public JsonResult Get()
+        {
+            return Json(new
+            {
+                status = "ok",
+                version = _config.config["version"],
+            });
+        }
+    }
+}
